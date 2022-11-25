a766b7aae84720412a6a79c6c789d0becafd0eff
Jordy Salazar <jordysalazar350@gmail.com>
2022-11-24 22:21:56 -0500

    segundo push

---
+++ script.js
@@ -0,0 +1,7 @@
+const cachudo = ()=>{
+    const pregunta=prompt("¿Cuantas veces te dijo que salia con sus amigas a una pijamada y regresaba con su cabello mojado?")
+  if (pregunta) {
+    return alert("aea cachudo de mierda")
+  }     
+}
+cachudo()
